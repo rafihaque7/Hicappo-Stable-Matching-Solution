e8f76c3a29682fa5e2fe0ce082c5b7122dccf5fc
shafihaque7 <shafihaque7@gmail.com>
2018-01-26 20:24:25 -0800

    Everything should work

---
+++ StableMarriage/FormFemaleName.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StableMarriage
+{
+    public partial class FormFemaleName : Form
+    {
+        ListView listViewFemale;
+        Label labelCurrentFemale;
+        public FormFemaleName(ListView listViewFemale, Label labelCurrentFemale)
+        {
+            InitializeComponent();
+            this.listViewFemale = listViewFemale;
+            this.labelCurrentFemale = labelCurrentFemale;
+        }
+
+        private void buttonAddFemale_Click(object sender, EventArgs e)
+        {
+            listViewFemale.Items.Add(textFemaleName.Text);
+            textFemaleName.Text = null;
+
+            labelCurrentFemale.Text = listViewFemale.Items.Count.ToString();
+            checkCountforFemale();
+        }
+
+        private void textFemaleName_TextChanged(object sender, EventArgs e)
+        {
+            this.AcceptButton = buttonAddFemale;
+        }
+
+        private void checkCountforFemale()
+        {
+            if (listViewFemale.Items.Count == FormMainInput.halfCount)
+            {
+                Close();
+            }
+        }
+    }
+}
