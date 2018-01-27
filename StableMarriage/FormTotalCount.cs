cb5153f1c5cf5fea22163cf63d8c458c5f3a87ed
shafihaque7 <rafihaque7@gmail.com>
2018-01-26 20:29:37 -0800

    under new contrib

---
+++ StableMarriage/FormTotalCount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StableMarriage
+{
+    public partial class FormTotalCount : Form
+    {
+        
+        public FormTotalCount()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTotalCount_Click(object sender, EventArgs e)
+        {
+
+            int totalCount = 0;
+            try
+            {
+                totalCount = Convert.ToInt32(textTotalCount.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            if (totalCount % 2 == 0)
+            {
+                string msg = "So " + totalCount / 2 + " Boys and " + totalCount / 2 + " Girls!";
+                MessageBox.Show(msg);
+                FormMainInput formMainInput = new FormMainInput(totalCount);
+                Hide();
+                formMainInput.ShowDialog();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("The number is not even or valid");
+            }
+
+
+        
+        }
+
+        private void textTotalCount_TextChanged(object sender, EventArgs e)
+        {
+            this.AcceptButton = buttonTotalCount;
+        }
+    }
+}
---
+++ StableMarriage/FormWelcome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StableMarriage
+{
+    public partial class FormWelcome : Form
+    {
+        public FormWelcome()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGetstarted_Click(object sender, EventArgs e)
+        {
+            FormTotalCount formTotalCount = new FormTotalCount();
+            Hide();
+            formTotalCount.ShowDialog();
+            Close();
+        }
+    }
+}
